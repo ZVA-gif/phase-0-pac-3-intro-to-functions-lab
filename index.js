69ddb93ac60c0d3c3739c45d26d71b73a32b0232
zoeanderson <zvann813@gmail.com>
2022-07-25 14:48:00 -0600

    commit tree 1b0afec3c5099aacc5a516d06b466182434ed1f4

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+    // todo
+  }
+  function shout(string) {
+    return string;
+  }
+  function shout(string) {
+    return string.toUpperCase();
+  }
+  function whisper(string) {
+    // todo
+  }
+  function whisper(string) {
+    return string;
+  }
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+console.log(string);
+console.log(string.toUpperCase());
+
+console.log(string === string.toUpperCase());
